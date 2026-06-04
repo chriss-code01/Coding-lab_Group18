9244dff7a6996effe13c9cb930c36e6f75ca9ac1
Michel Nerlin Cyusa <m.cyusa@alustudent.com>
2026-06-04 13:56:43 -0700

    Created secure_data function for log directory protection

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -40,7 +40,7 @@ secure_data() {
         echo "Error: active_logs directory not found."
     fi
 
-    echo "Security permissions applied successfully."
+    echo "Security permissions applied."
 }
 
 
